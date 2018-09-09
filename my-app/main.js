8c37a24935ea4750c65e2afae8bd10c1e9cde083
liyuqun <94543-71571@163.com>
2018-09-09 18:52:09 +0800

    markdown

--- my-app/main.js
+++ my-app/main.js
@@ -30,7 +30,7 @@ webpackEmptyAsyncContext.id = "./src/$$_lazy_route_resource lazy recursive";
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-module.exports = "\r\n#nav-left{\r\n    float: left;\r\n    height: 100vh;\r\n    width: 30px;\r\n    border: 1px solid green;\r\n}\r\n#nav-right {\r\n    float: right;\r\n    height: 100vh;\r\n    width: calc(100% - 35px);\r\n    border: 1px solid blue;\r\n    display: inline-flex;\r\n    flex-direction: column;\r\n    justify-content: space-between;\r\n}\r\n#nav-top{\r\n    height: 30px;\r\n    border: 1px solid green;\r\n}\r\n#main{\r\n    margin-left: 30px;\r\n}\r\n#nav-footer{\r\n    height: 30px;\r\n    border: 1px solid green;\r\n}"
+module.exports = "\r\n#nav-left{\r\n    float: left;\r\n    min-height: 100vh;\r\n    width: 30px;\r\n    border: 1px solid green;\r\n}\r\n#nav-right {\r\n    float: right;\r\n    min-height: 100vh;\r\n    width: calc(100% - 35px);\r\n    border: 1px solid blue;\r\n    display: inline-flex;\r\n    flex-direction: column;\r\n    justify-content: space-between;\r\n}\r\n#nav-top{\r\n    height: 30px;\r\n    border: 1px solid green;\r\n}\r\n#main{\r\n    margin-left: 30px;\r\n}\r\n#nav-footer{\r\n    height: 30px;\r\n    border: 1px solid green;\r\n}"
 
 /***/ }),
 
@@ -41,7 +41,7 @@ module.exports = "\r\n#nav-left{\r\n    float: left;\r\n    height: 100vh;\r\n  
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-module.exports = "<!-- 左边导航（小内容） -->\n<nav id=\"nav-left\">nav</nav>\n<!-- 右边内容（小导航） -->\n<nav id=\"nav-right\">\n    <header id=\"nav-top\">\n        Header\n    </header>\n    <main id=\"main\" style=\"text-align:center\">\n        <h1>\n          Welcome to {{ title }}!\n        </h1>\n        <!-- <img width=\"300\" alt=\"Angular Logo\" src=\"data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNTAgMjUwIj4KICAgIDxwYXRoIGZpbGw9IiNERDAwMzEiIGQ9Ik0xMjUgMzBMMzEuOSA2My4ybDE0LjIgMTIzLjFMMTI1IDIzMGw3OC45LTQzLjcgMTQuMi0xMjMuMXoiIC8+CiAgICA8cGF0aCBmaWxsPSIjQzMwMDJGIiBkPSJNMTI1IDMwdjIyLjItLjFWMjMwbDc4LjktNDMuNyAxNC4yLTEyMy4xTDEyNSAzMHoiIC8+CiAgICA8cGF0aCAgZmlsbD0iI0ZGRkZGRiIgZD0iTTEyNSA1Mi4xTDY2LjggMTgyLjZoMjEuN2wxMS43LTI5LjJoNDkuNGwxMS43IDI5LjJIMTgzTDEyNSA1Mi4xem0xNyA4My4zaC0zNGwxNy00MC45IDE3IDQwLjl6IiAvPgogIDwvc3ZnPg==\"> -->\n\n        <h1>\n          Welcome to {{ title }}! 875758\n        </h1>\n\n        <h1>\n          Welcome to {{ title }}!\n        </h1>\n    </main>\n    <footer id=\"nav-footer\">\n        Footer\n    </footer>\n</nav>"
+module.exports = "<!-- 左边导航（小内容） -->\n<nav id=\"nav-left\">nav</nav>\n<!-- 右边内容（小导航） -->\n<nav id=\"nav-right\">\n    <header id=\"nav-top\">\n        Header\n    </header>\n    <main id=\"main\" style=\"text-align:center\">\n        <h1>\n          Welcome to {{ title }}!\n          <div [markdown]='test'></div>\n        </h1>\n        <!-- <img width=\"300\" alt=\"Angular Logo\" src=\"data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNTAgMjUwIj4KICAgIDxwYXRoIGZpbGw9IiNERDAwMzEiIGQ9Ik0xMjUgMzBMMzEuOSA2My4ybDE0LjIgMTIzLjFMMTI1IDIzMGw3OC45LTQzLjcgMTQuMi0xMjMuMXoiIC8+CiAgICA8cGF0aCBmaWxsPSIjQzMwMDJGIiBkPSJNMTI1IDMwdjIyLjItLjFWMjMwbDc4LjktNDMuNyAxNC4yLTEyMy4xTDEyNSAzMHoiIC8+CiAgICA8cGF0aCAgZmlsbD0iI0ZGRkZGRiIgZD0iTTEyNSA1Mi4xTDY2LjggMTgyLjZoMjEuN2wxMS43LTI5LjJoNDkuNGwxMS43IDI5LjJIMTgzTDEyNSA1Mi4xem0xNyA4My4zaC0zNGwxNy00MC45IDE3IDQwLjl6IiAvPgogIDwvc3ZnPg==\"> -->\n\n        <h1>\n          Welcome to {{ title }}! #875758#\n        </h1>\n\n        <h1>\n          Welcome to {{ title }}!\n        </h1>\n    </main>\n    <footer id=\"nav-footer\">\n        Footer\n    </footer>\n</nav>"
 
 /***/ }),
 
@@ -66,6 +66,7 @@ var __decorate = (undefined && undefined.__decorate) || function (decorators, ta
 var AppComponent = /** @class */ (function () {
     function AppComponent() {
         this.title = '技术';
+        this.test = "#Test Markdown #\n*doc is here*\n##markdown js";
     }
     AppComponent = __decorate([
         Object(_angular_core__WEBPACK_IMPORTED_MODULE_0__["Component"])({
@@ -93,7 +94,8 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "AppModule", function() { return AppModule; });
 /* harmony import */ var _angular_platform_browser__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @angular/platform-browser */ "./node_modules/@angular/platform-browser/fesm5/platform-browser.js");
 /* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @angular/core */ "./node_modules/@angular/core/fesm5/core.js");
-/* harmony import */ var _app_component__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./app.component */ "./src/app/app.component.ts");
+/* harmony import */ var _directive_directive_module__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./directive/directive.module */ "./src/app/directive/directive.module.ts");
+/* harmony import */ var _app_component__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./app.component */ "./src/app/app.component.ts");
 var __decorate = (undefined && undefined.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
@@ -103,22 +105,115 @@ var __decorate = (undefined && undefined.__decorate) || function (decorators, ta
 
 
 
+
 var AppModule = /** @class */ (function () {
     function AppModule() {
     }
     AppModule = __decorate([
         Object(_angular_core__WEBPACK_IMPORTED_MODULE_1__["NgModule"])({
             declarations: [
-                _app_component__WEBPACK_IMPORTED_MODULE_2__["AppComponent"]
+                _app_component__WEBPACK_IMPORTED_MODULE_3__["AppComponent"]
             ],
             imports: [
-                _angular_platform_browser__WEBPACK_IMPORTED_MODULE_0__["BrowserModule"]
+                _angular_platform_browser__WEBPACK_IMPORTED_MODULE_0__["BrowserModule"],
+                _directive_directive_module__WEBPACK_IMPORTED_MODULE_2__["DirectiveModule"]
             ],
             providers: [],
-            bootstrap: [_app_component__WEBPACK_IMPORTED_MODULE_2__["AppComponent"]]
+            bootstrap: [_app_component__WEBPACK_IMPORTED_MODULE_3__["AppComponent"]]
         })
     ], AppModule);
     return AppModule;
+}());
+
+
+
+/***/ }),
+
+/***/ "./src/app/directive/directive.module.ts":
+/*!***********************************************!*\
+  !*** ./src/app/directive/directive.module.ts ***!
+  \***********************************************/
+/*! exports provided: DirectiveModule */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "DirectiveModule", function() { return DirectiveModule; });
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @angular/core */ "./node_modules/@angular/core/fesm5/core.js");
+/* harmony import */ var _markdown_directive__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./markdown.directive */ "./src/app/directive/markdown.directive.ts");
+var __decorate = (undefined && undefined.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+
+
+var DirectiveModule = /** @class */ (function () {
+    function DirectiveModule() {
+    }
+    DirectiveModule = __decorate([
+        Object(_angular_core__WEBPACK_IMPORTED_MODULE_0__["NgModule"])({
+            declarations: [
+                _markdown_directive__WEBPACK_IMPORTED_MODULE_1__["MarkdownDirective"]
+            ],
+            exports: [
+                _markdown_directive__WEBPACK_IMPORTED_MODULE_1__["MarkdownDirective"]
+            ]
+        })
+    ], DirectiveModule);
+    return DirectiveModule;
+}());
+
+
+
+/***/ }),
+
+/***/ "./src/app/directive/markdown.directive.ts":
+/*!*************************************************!*\
+  !*** ./src/app/directive/markdown.directive.ts ***!
+  \*************************************************/
+/*! exports provided: MarkdownDirective */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "MarkdownDirective", function() { return MarkdownDirective; });
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @angular/core */ "./node_modules/@angular/core/fesm5/core.js");
+/* harmony import */ var markdown__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! markdown */ "./node_modules/markdown/lib/index.js");
+/* harmony import */ var markdown__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(markdown__WEBPACK_IMPORTED_MODULE_1__);
+var __decorate = (undefined && undefined.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (undefined && undefined.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+
+
+var MarkdownDirective = /** @class */ (function () {
+    function MarkdownDirective(el) {
+        this.el = el;
+        this.mdStr = "";
+        this.md = markdown__WEBPACK_IMPORTED_MODULE_1__["markdown"];
+    }
+    MarkdownDirective.prototype.ngOnInit = function () {
+        console.info(this.mdStr);
+        this.el.nativeElement.innerHTML = this.md.toHTML(this.mdStr);
+    };
+    __decorate([
+        Object(_angular_core__WEBPACK_IMPORTED_MODULE_0__["Input"])("markdown"),
+        __metadata("design:type", String)
+    ], MarkdownDirective.prototype, "mdStr", void 0);
+    MarkdownDirective = __decorate([
+        Object(_angular_core__WEBPACK_IMPORTED_MODULE_0__["Directive"])({
+            selector: "[markdown]"
+        }),
+        __metadata("design:paramtypes", [_angular_core__WEBPACK_IMPORTED_MODULE_0__["ElementRef"]])
+    ], MarkdownDirective);
+    return MarkdownDirective;
 }());
 
 
